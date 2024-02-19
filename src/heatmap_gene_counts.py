15414b995a08e0e0f1b23e97383776d079a0e9ff
Luis Pedro Coelho <luis@luispedro.org>
2024-02-20 09:40:42 +1000

    ENH Generate co-occurrence plots for all tools

--- src/heatmap_gene_counts.py
+++ src/heatmap_gene_counts.py
@@ -6,44 +6,47 @@ import lzma
 import seaborn as sns
 from scipy import cluster, spatial
 
-from load_data import load_data
+from load_data import load_data, ALL_TOOLS
 
-TOOL = 'rgi_strict'
 HABITAT = 'human gut'
 
-df = load_data(TOOL, HABITAT)
+for tool in ALL_TOOLS:
+    df = load_data(tool, HABITAT)
 
-freq = df.select(cs.by_dtype(pl.Int64)) \
-        .select(pl.col("*") > 0) \
-        .sum()
+    freq = df.select(cs.by_dtype(pl.Int64)) \
+            .select(pl.col("*") > 0) \
+            .sum()
 
-nr_samples = df.shape[0]
-min_freq = nr_samples // 5
-max_freq = nr_samples - min_freq
+    nr_samples = df.shape[0]
+    min_freq = nr_samples // 5
+    max_freq = nr_samples - min_freq
 
-interesting = [col.name for col in
-                    freq.select((pl.col("*") > min_freq) & (pl.col("*") < max_freq))
-                if col.all()]
-nr_mid_freq_genes = len(interesting)
+    interesting = [col.name for col in
+                        freq.select((pl.col("*") > min_freq) & (pl.col("*") < max_freq))
+                    if col.all()]
+    nr_mid_freq_genes = len(interesting)
 
-counts = np.zeros((nr_mid_freq_genes, nr_mid_freq_genes), dtype=np.int64)
-for i in range(nr_mid_freq_genes):
-    counts[i,i] = df.select(interesting[i]).select(pl.col("*") > 0).sum()[0,0]
-    for j in range(i + 1, nr_mid_freq_genes):
-        mask = df.select(
-                [interesting[i], interesting[j]]) \
-                .select(pl.col("*") > 0) \
-                .sum_horizontal()
-        both = (mask == 2).sum()
-        counts[i,j] = both
-        counts[j,i] = both
+    print(f"{tool}: {df.shape[0]} samples x {df.shape[1]} genes ({nr_mid_freq_genes} genes with 20-80% presence)")
+    counts = np.zeros((nr_mid_freq_genes, nr_mid_freq_genes), dtype=np.int64)
+    for i in range(nr_mid_freq_genes):
+        counts[i,i] = df.select(interesting[i]).select(pl.col("*") > 0).sum()[0,0]
+        for j in range(i + 1, nr_mid_freq_genes):
+            mask = df.select(
+                    [interesting[i], interesting[j]]) \
+                    .select(pl.col("*") > 0) \
+                    .sum_horizontal()
+            both = (mask == 2).sum()
+            counts[i,j] = both
+            counts[j,i] = both
 
-fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-Z = cluster.hierarchy.linkage(counts, method='average', optimal_ordering=True)
-R = cluster.hierarchy.dendrogram(Z, ax=ax, labels=interesting, leaf_rotation=90, orientation='left')
-leaves = R['leaves']
-ax.clear()
-sns.heatmap(counts[leaves].T[leaves] + 1, ax=ax, cmap='viridis')
-fig.tight_layout()
+    Z = cluster.hierarchy.linkage(counts, method='average', optimal_ordering=True)
+    R = cluster.hierarchy.dendrogram(Z, ax=ax, labels=interesting, leaf_rotation=90, orientation='left')
+    leaves = R['leaves']
+    ax.clear()
+    sns.heatmap(counts[leaves].T[leaves] + 1, ax=ax, cmap='viridis')
+    ax.set_title(f'Gene co-occurrence in {tool} ({HABITAT})')
+    fig.tight_layout()
+    fig.savefig(f'plots/heatmap_{tool}_{HABITAT}.pdf')
 
