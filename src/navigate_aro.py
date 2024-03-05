e0bef147b5950d01eeaf3547c9b35731ad06cc03
Luis Pedro Coelho <luis@luispedro.org>
2024-03-05 10:05:28 +1000

    2024-03-05: map deepARG to ARO
    - using argNorm

--- src/navigate_aro.py
+++ src/navigate_aro.py
@@ -5,12 +5,17 @@ import polars.selectors as cs
 import numpy as np
 import lzma
 import seaborn as sns
-from collections import Counter
+from scipy import cluster
+from collections import defaultdict, Counter
 
+import argnorm.normalizers
 from load_data import load_data, ALL_TOOLS
 
 HABITAT = 'human gut'
-df = load_data('rgi_strict', HABITAT)
+tool = 'rgi_strict'
+tool = 'deeparg'
+
+df = load_data(tool, HABITAT)
 freq = df.select(cs.by_dtype(pl.Int64)) \
         .select(pl.col("*") > 0) \
         .sum()
@@ -36,7 +41,6 @@ for i in range(nr_mid_freq_genes):
         counts[j,i] = both
 
 %matplotlib qt
-tool = 'rgi_strict'
 
 fig, ax = plt.subplots()
 Z = cluster.hierarchy.linkage(counts, method='average', optimal_ordering=True)
@@ -49,8 +53,6 @@ fig.tight_layout()
 interesting_reordered = [interesting[i] for i in leaves]
 
 
-
-
 aro = pronto.Ontology('aro.obo')
 name2term = {}
 for k in aro.keys():
@@ -58,14 +60,23 @@ for k in aro.keys():
         name2term[aro[k].name] = k
 
 terms = []
-for g in interesting_reordered[280:300]:
+for g in interesting_reordered[:45]:
     if g in name2term:
         terms.append(aro[name2term[g]])
 
-cs = Counter()
-for term in terms:
-    cs.update(term.superclasses())
+norm = argnorm.normalizers.DeepARGNormalizer()
+aro_map_table = norm.get_aro_mapping_table()
+aro_map_table['GeneFamily'] = aro_map_table['Original ID'].str.split('|').str[4].str.upper()
 
-for term, count in cs.most_common(10):
-    if count < 19:
-        print(f'{term.name} ({term.id}): {count}')
+cluster = interesting_reordered[:45]
+
+by_family = defaultdict(set)
+for _,row in aro_map_table[['GeneFamily', 'ARO']].iterrows():
+    by_family[row.GeneFamily].add(row.ARO)
+
+for c in cluster:
+    print(c)
+    for a in by_family[c]:
+        print(f'  {aro[a].name} ({a})')
+        for s in aro[a].superclasses():
+            print(f'         {s.name} ({s.id})')
